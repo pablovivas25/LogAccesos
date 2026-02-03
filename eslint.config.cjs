23f0296558c1c0c6687a68713cb333164a3df458
pablovivas25 <vivas.p52@gmail.com>
2026-02-02 21:25:20 -0300

    configuracion lint

---
+++ eslint.config.cjs
@@ -0,0 +1,18 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      "no-console": "off"
+    }
+  }
+];
